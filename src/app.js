49995a7be450f80d96325ec7f2b3dcb2479b4236
Derek <derekluu90@gmail.com>
2020-08-12 15:39:17 -0400

    replace URL constructor with DNS module

--- src/app.js
+++ src/app.js
@@ -2,46 +2,48 @@ require('./db/mongoose.js')
 const express = require('express')
 const bodyParser = require('body-parser')
 const UrlModel = require('./models/url')
-const ValidateURL = require('url').URL
+const dns = require('dns')
+// const ValidateURL = require('url').URL
 const { default: ShortUniqueId } = require('short-unique-id')
 
 const app = express()
 const jsonParser = bodyParser.json()
-
 const port = process.env.PORT || 3000
 
-app.post('/api/shorturl/new', jsonParser, async (req, res) => {
+app.post('/api/shorturl/new', jsonParser, (req, res) => {
   const url = req.body.url
-  // validates URL
-  try {
-    // constructor checks for valid URL, if invalid error is thrown
-    new ValidateURL(url)
-  } catch (e) {
-    // catch above error for when invalid URL is provided
-    res.status(400).send({ error: "Invalid URL"})
-  }
-  
-  // interacts with database
-  try {
-    // queries are async, check if url already exists
-    const existingUrl = await UrlModel.findOne({ url })
-    const hostname = req.hostname === 'localhost' ? `${req.hostname}:${port}` : req.hostname
-    const shortUrl = `${hostname}/shorturl`
-    // if it does not exist, create a new document and save
-    if (!existingUrl){
-      const uid = new ShortUniqueId()
-      const newUrl = new UrlModel({ url, hash: uid(10) })
-      await newUrl.save()
-      res.send({url: `${shortUrl}/${newUrl.hash}`})
-    } else {
-      // if it exists, send in json
-      res.send({url: `${shortUrl}/${existingUrl.hash}`})
-    }
-  } catch (e){
-    res.status(500).send(e)
-  }
-})
+  // parse string to get hostname
+  let parsedUrl = url.match('https') ? url.replace('https://', '') : url.replace('http://', '')
+  const urlIndex = parsedUrl.indexOf('/')
+  parsedUrl = urlIndex > - 1 ? parsedUrl.substring(0, urlIndex) : parsedUrl
 
+  // validates hostname
+  dns.lookup(parsedUrl, async (error) => {
+    if(error){
+      return res.send({ url: "Invalid Url" })
+    }
+
+    // interacts with database
+    try {
+      // queries are async, check if url already exists
+      const existingUrl = await UrlModel.findOne({ url })
+      const hostname = req.hostname === 'localhost' ? `${req.hostname}:${port}` : req.hostname
+      const shortUrl = `${hostname}/shorturl`
+      // if it does not exist, create a new document and save
+      if (!existingUrl){
+        const uid = new ShortUniqueId() 
+        const newUrl = new UrlModel({ url, hash: uid(10) })
+        await newUrl.save()
+        res.send({url: `${shortUrl}/${newUrl.hash}`})
+      } else {
+        // if url exists, send back url
+        res.send({url: `${shortUrl}/${existingUrl.hash}`})
+      }
+    } catch (e){
+      res.status(500).send(e)
+    }
+  })
+})
 
 // get request to redirect the URL
 app.listen(port, () => {
